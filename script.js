19b4346e79fa00a506c9b7f6a2d68471dc8baae9
GustavoRizzo <grizzo.albu@gmail.com>
2023-01-12 18:35:49 -0300

    Primeiro exemplo funcional

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('oi, eu sou a sua extenção fb-ads-finder');
+
+replaceText(document.body);
+
+function replaceText(element){
+
+    if(element.hasChildNodes()) {
+        element.childNodes.forEach(replaceText) //recursivamente vai chamando as funções para os filhos dos elementos
+    } else if (element.nodeType === Text.TEXT_NODE) {
+        if (element.textContent.match(/COVID/gi)) {
+            element.parentElement.style.backgroundColor = 'pink';
+        }
+        /* Exemplo replace text
+        if (element.nodeType === Text.TEXT_NODE) {
+            element.textContent = element.textContent.replace(/COVID/gi, 'OLÁ MUNDO!');
+            element.parentElement.style.backgroundColor = 'pink';
+        } */
+    }
+}
+    
+    
+    /* Exemplo replace text
+    if (element.nodeType === Text.TEXT_NODE) {
+        element.textContent = element.textContent.replace(/COVID/gi, 'OLÁ MUNDO!');
+        element.parentElement.style.backgroundColor = 'pink';
+    } */
+
+
+
